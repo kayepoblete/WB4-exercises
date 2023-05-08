c3cd12225647b486594e9328615005f047edf83e
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-05-08 13:37:03 -0700

    Created product_code.js
    Created product_code.js

---
+++ ObjectScripts/product_code.js
@@ -0,0 +1,22 @@
+// Exercise 2 page 14
+"use strict";
+
+function parsePartCode(productCode) {
+    let colonPos = productCode.indexOf(":");
+    let dashPos = productCode.indexOf("-");
+    let suppCode = productCode.substring(0, colonPos);
+    let prodNum = productCode.substring(colonPos+1, dashPos)
+    let sizeCode = productCode.substring(dashPos+1);
+    let code = {
+        supplierCode: suppCode,
+        productNumber: prodNum,
+        size: sizeCode
+    }
+    return code;
+}
+
+let partCode1 = "XYZ:1234-L";
+let part1 = parsePartCode(partCode1);
+console.log("Supplier: " + part1.supplierCode +
+ " Product Number: " + part1.productNumber +
+ " Size: " + part1.size);
